30aac1458a312503b0fd8e6b29e1e4a352b55631
Anna <anna.ovsi@mail.ru>
2020-10-01 14:35:10 +0300

    bug

--- detect_car_license_plates.py
+++ detect_car_license_plates.py
@@ -12,19 +12,13 @@ from regions import *
 
 all_regions = load_regions()
 print(all_regions)
-#image = cv2.imread("test.jpg")
-#cv2.imshow('sd', image)
 
 import model
-#state, textArr, status = model.detect_number(image, "A001MP05")
-#print(state, textArr, status)
 
-cap = cv2.VideoCapture("Y618XX123.mp4")
+cap = cv2.VideoCapture("video.mp4")
 
 while(cap.isOpened()):
     ret, frame = cap.read()
-    state, textArr, status = model.detect_number(frame, "Y618XX123")
-    print(status)
-    #cv2.imshow('video', frame)
-#cap.release()
-#cv2.destroyAllWindows()
+    state, textArr, status = model.detect_number(frame, "no name")
+    print(status, textArr)
+
