a364854e2d462d60542bdc0f013936dceed8ff57
Anna <anna.ovsi@mail.ru>
2020-10-02 12:29:08 +0300

    bug

--- read_video.py
+++ read_video.py
@@ -3,15 +3,18 @@ import model
 
 def detect_one_video(video, name=" "):
     count = 0
+    cadr = 0
     cap = cv2.VideoCapture(video)
 
     while cap.isOpened():
         ret, frame = cap.read()
+        cadr += 1
         state, number, status = model.detect_number(frame, name)
         if state:
             print(count, number)
             count = 0
         else:
+            print("nothing", cadr)
             count += 1
 
     cap.release()
