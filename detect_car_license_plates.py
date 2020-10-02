f76a259a87724c4df05a60d3f7f74e105e8dba8d
Anna <anna.ovsi@mail.ru>
2020-10-02 12:39:33 +0300

    bug

--- detect_car_license_plates.py
+++ detect_car_license_plates.py
@@ -1,16 +1,32 @@
-import os
-import numpy as np
-import sys
-import matplotlib.image as mpimg
-from matplotlib import pyplot as plt
-import cv2
 import warnings
 warnings.filterwarnings('ignore')
 import read_video
+import logging
+import sys
+logger = logging.getLogger("detect")
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s %(levelname)s:%(message)s', datefmt='%d-%b-%y %H:%M:%S')
+
+fh = logging.FileHandler('logs.log')
+fh.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler(stream=sys.stdout)
+ch.setLevel(logging.DEBUG)
+
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+logger.addHandler(fh)
+logger.addHandler(ch)
+
 
 #os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 PATH_VIDEO = "video.mp4"
+logger.debug("Запустили видео ", PATH_VIDEO)
 read_video.detect_one_video(PATH_VIDEO, "no name")
-#read_video.detect_one_video("Y618XX123.mp4", "Y618XX123")
 
+PATH_VIDEO = "video2.mp4"
+logger.debug("Запустили видео ", PATH_VIDEO)
+read_video.detect_one_video(PATH_VIDEO, "no name")
--- read_video.py
+++ read_video.py
@@ -1,5 +1,23 @@
 import cv2
 import model
+import logging
+import sys
+logger = logging.getLogger("detect")
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s %(levelname)s:%(message)s', datefmt='%d-%b-%y %H:%M:%S')
+
+fh = logging.FileHandler('logs.log')
+fh.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler(stream=sys.stdout)
+ch.setLevel(logging.DEBUG)
+
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+logger.addHandler(fh)
+logger.addHandler(ch)
 
 def detect_one_video(video, name=" "):
     count = 0
@@ -11,10 +29,11 @@ def detect_one_video(video, name=" "):
         cadr += 1
         state, number, status = model.detect_number(frame, name)
         if state:
-            print(count, number)
+            logger.debug(count, number)
             count = 0
         else:
-            print("nothing", cadr)
+            if cadr % 10 == 0:
+                logger.debug("nothing", cadr)
             count += 1
 
     cap.release()
