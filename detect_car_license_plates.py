cdc268737c4083b842107d7c8a45d5b1fdf118fa
Anna <anna.ovsi@mail.ru>
2020-10-03 15:37:43 +0300

    in developing..

--- detect_car_license_plates.py
+++ detect_car_license_plates.py
@@ -5,17 +5,12 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
 
-#PATH_VIDEO = "video4.mp4"
-#logging.info(" Запустили видео %s" % PATH_VIDEO)
-#cars = read_video.detect_one_video(PATH_VIDEO, "no name")
-#logging.info(" Итоговый номер " + str(cars) + "\n\n")
-
-PATH_VIDEO = "B678XY178_T576HB123_1.57.mp4"
+PATH_VIDEO = "video.mp4"
 logging.info(" Запустили видео %s" % PATH_VIDEO)
 cars = read_video.detect_one_video(PATH_VIDEO, "no name")
-logging.info(" Итоговый номер " + str(set(cars)))
+logging.info(" Итоговый номер " + str(set(cars)) + "\n\n")
 
-#PATH_VIDEO = "video.mp4"
-#logging.info(" Запустили видео %s" % PATH_VIDEO)
-#cars = read_video.detect_one_video(PATH_VIDEO, "no name")
-#logging.info(" Итоговый номер " + str(cars))
+PATH_VIDEO = "T576HB123_Y663YO750_3.36_720.mp4"
+logging.info(" Запустили видео %s" % PATH_VIDEO)
+cars = read_video.detect_one_video(PATH_VIDEO, "no name")
+logging.info(" Итоговый номер " + str(set(cars)) + "\n\n")
