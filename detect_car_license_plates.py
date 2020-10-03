abcfc387c4f0c5fe9e922c241caed272e6214041
Anna <anna.ovsi@mail.ru>
2020-10-03 17:00:25 +0300

    in developing..

--- detect_car_license_plates.py
+++ detect_car_license_plates.py
@@ -3,24 +3,24 @@ warnings.filterwarnings('ignore')
 import read_video
 import logging
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s:%(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
 
 #PATH_VIDEO = "http://62.94.249.83/axis-cgi/mjpg/video.cgi"
 #logging.info(" Запустили видео %s" % PATH_VIDEO)
 #cars = read_video.detect_one_video(PATH_VIDEO, "no name")
 #logging.info(" Итоговый номер " + str(set(cars)) + "\n\n")
 
-PATH_VIDEO = "video.mp4"
+PATH_VIDEO = "video4.mp4"
 logging.info(" Запустили видео %s" % PATH_VIDEO)
 cars = read_video.detect_one_video(PATH_VIDEO, "no name")
 logging.info(" Итоговый номер " + str(set(cars)) + "\n\n")
 
-#PATH_VIDEO = "T576HB123_Y663YO750_3.36_720.mp4"
-#logging.info(" Запустили видео %s" % PATH_VIDEO)
-#cars = read_video.detect_one_video(PATH_VIDEO, "no name")
-#logging.info(" Итоговый номер " + str(set(cars)) + "\n\n")
+PATH_VIDEO = "T576HB123_Y663YO750_3.36_720.mp4"
+logging.info(" Запустили видео %s" % PATH_VIDEO)
+cars = read_video.detect_one_video(PATH_VIDEO, "no name")
+logging.info(" Итоговый номер " + str(set(cars)) + "\n\n")
 
-#PATH_VIDEO = "B678XY178_T576HB123_1.57.mp4"
-#logging.info(" Запустили видео %s" % PATH_VIDEO)
-#cars = read_video.detect_one_video(PATH_VIDEO, "no name")
-#logging.info(" Итоговый номер " + str(set(cars)) + "\n\n")
+PATH_VIDEO = "B678XY178_T576HB123_1.57.mp4"
+logging.info(" Запустили видео %s" % PATH_VIDEO)
+cars = read_video.detect_one_video(PATH_VIDEO, "no name")
+logging.info(" Итоговый номер " + str(set(cars)) + "\n\n")
--- read_video.py
+++ read_video.py
@@ -3,7 +3,7 @@ import model
 import wrong_numbers
 
 import logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s:%(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
 
 MIN_CADRS_TO_DETECT = 2
 CADRS_TO_FIND_NEW_CAR = 10
