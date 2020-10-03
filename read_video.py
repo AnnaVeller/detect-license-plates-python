10746e0226552c93f9e415f7c6bfdb9ea66443df
Anna <anna.ovsi@mail.ru>
2020-10-03 13:40:25 +0300

    bug

--- read_video.py
+++ read_video.py
@@ -30,17 +30,17 @@ def detect_one_video(video, name=" "):
             if count < 4:
                 one_number.extend(number)
                 logging.debug(" список one_number " + str(one_number))
+            elif count == 4:      # прошло 4 кадра после обнаружения знака
+                name = wrong_numbers.wrong(one_number)
+                car_list.append(name)
+                #logging.info(" текущий список номеров " + str(car_list))
             else:
-                if count == 4:      # прошло 4 кадра после обнаружения знака
-                    name = wrong_numbers.wrong(one_number)
-                    car_list.append(name)
-                    logging.debug(" текущий список номеров " + str(car_list))
-                else:
-                    one_number.clear()
+                one_number.clear()
     if count < 4:       # если видео закончилось на кадре где есть машина
         name = wrong_numbers.wrong(one_number)
         logging.debug(" текущий номер " + str(name))
         car_list.append(name)
+        logging.info(" текущий список номеров " + str(car_list))
     cap.release()
     cv2.destroyAllWindows()
     return car_list
