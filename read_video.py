94ca3ad065db4cf23a5ed3b94b3510dd64e7d718
Anna <anna.ovsi@mail.ru>
2020-10-03 14:46:42 +0300

    in developing..

--- read_video.py
+++ read_video.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(mess
 
 
 def detect_one_video(video, name=" "):
-    count = 0
+    count = 100000
     cadr = 0
     cap = cv2.VideoCapture(video)
     one_number = []
@@ -27,7 +27,7 @@ def detect_one_video(video, name=" "):
                 if count % 10 == 0:     # чтобы не выводить слишком часто отладочные сообщения
                     logging.debug(" Номер не найден. Обработали %d кадр" % cadr)
                 count += 1
-            if count < 4 and cadr > 4:     # чтобы это не сработало на первых 4 кадрах
+            if count < 4:     # чтобы это не сработало на первых 4 кадрах
                 one_number.extend(number)
                 logging.debug(" список one_number " + str(one_number))
             elif count == 4:      # прошло 4 кадра после обнаружения знака
