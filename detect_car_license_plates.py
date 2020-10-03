569d4b610349787607bf93dd346f9627e7dce673
Anna <anna.ovsi@mail.ru>
2020-10-03 11:09:27 +0300

    bug

--- detect_car_license_plates.py
+++ detect_car_license_plates.py
@@ -6,17 +6,16 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s:%(message)s')
 
 PATH_VIDEO = "video4.mp4"
-logging.debug(" Запустили видео %s" % PATH_VIDEO)
+logging.info(" Запустили видео %s" % PATH_VIDEO)
 cars = read_video.detect_one_video(PATH_VIDEO, "no name")
 logging.info(" Итоговый номер " + str(cars) + "\n\n")
 
-PATH_VIDEO = "video4.mp4"
-logging.debug(" Запустили видео %s" % PATH_VIDEO)
+PATH_VIDEO = "video2.mp4"
+logging.info(" Запустили видео %s" % PATH_VIDEO)
 cars = read_video.detect_one_video(PATH_VIDEO, "no name")
-logging.info(" Итоговый номер " + str(cars) + "\n\n")
+logging.info(" Итоговый номер " + str(cars))
 
-
-#PATH_VIDEO = "video2.mp4"
-#logging.debug(" Запустили видео %s" % PATH_VIDEO)
-#cars = read_video.detect_one_video(PATH_VIDEO, "no name")
-#logging.info(" Итоговый номер " + str(cars))
+PATH_VIDEO = "video.mp4"
+logging.info(" Запустили видео %s" % PATH_VIDEO)
+cars = read_video.detect_one_video(PATH_VIDEO, "no name")
+logging.info(" Итоговый номер " + str(cars))
