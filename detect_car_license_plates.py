655118681c3db7a78bb800a6eb5ad23111d259ce
Anna <anna.ovsi@mail.ru>
2020-10-05 09:23:58 +0300

    in developing..

--- detect_car_license_plates.py
+++ detect_car_license_plates.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(mess
 #cars = read_video.detect_one_video(PATH_VIDEO, "no name")
 #logging.info(" Итоговый номер " + str(set(cars)) + "\n\n")
 
-PATH_VIDEO = "test_mini.mp4"
+PATH_VIDEO = "video.mp4"
 logging.info(" Запустили видео %s" % PATH_VIDEO)
 cars = read_video.detect_one_video(PATH_VIDEO, "no name")
 logging.info(" Итоговый номер " + str(set(cars)) + "\n\n")
