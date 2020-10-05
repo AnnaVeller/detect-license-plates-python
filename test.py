300e8581adf8e7c126fc2d151b6e5c7b5f76b445
Anna <anna.ovsi@mail.ru>
2020-10-05 10:15:15 +0300

    in developing..

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import cv2
 
 frame = cv2.imread("11.jpg")
 number = ["sdfsdfsd"]
-cv2.putText(frame, str(number), (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,255), 2)
+
+cv2.putText(frame, str(number), (200, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
 cv2.imshow("dd", frame)
 cv2.waitKey(0)
