6488e130dd59dc1365fc6d7923edca61a58e6df2
Anna <anna.ovsi@mail.ru>
2020-10-06 17:12:17 +0300

    in developing..

--- model.py
+++ model.py
@@ -1,6 +1,5 @@
 import os
 import sys
-from json2html import a
 from NomeroffNet import filters, RectDetector, TextDetector, OptionsDetector, Detector, \
     textPostprocessing, textPostprocessingAsync
 
