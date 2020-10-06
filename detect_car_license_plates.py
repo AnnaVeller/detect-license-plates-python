cc9624e64e57e7b3a2311684c30ea216bb84c742
Anna <anna.ovsi@mail.ru>
2020-10-06 15:21:21 +0300

    in developing..

--- detect_car_license_plates.py
+++ detect_car_license_plates.py
@@ -6,19 +6,7 @@ import os
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
 
-PATH_VIDEO = "video2.mp4"
-NAME = os.path.splitext(PATH_VIDEO)[0]
-logging.info(" Запустили видео %s" % PATH_VIDEO)
-cars = read_video.detect_one_video(PATH_VIDEO, NAME)
-logging.info(" Итоговый номер " + str(set(cars)) + "\n\n")
-
-PATH_VIDEO = "T576HB123_Y663YO750_3.36_720.mp4"
-NAME = os.path.splitext(PATH_VIDEO)[0]
-logging.info(" Запустили видео %s" % PATH_VIDEO)
-cars = read_video.detect_one_video(PATH_VIDEO, NAME)
-logging.info(" Итоговый номер " + str(set(cars)) + "\n\n")
-
-PATH_VIDEO = "B678XY178_T576HB123_1.57.mp4"
+PATH_VIDEO = "test_mini.mp4"
 NAME = os.path.splitext(PATH_VIDEO)[0]
 logging.info(" Запустили видео %s" % PATH_VIDEO)
 cars = read_video.detect_one_video(PATH_VIDEO, NAME)
