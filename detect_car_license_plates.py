db46f35929c2980b38475e076e30b093ceec3e79
Anna <anna.ovsi@mail.ru>
2020-10-06 17:38:27 +0300

    in developing..

--- detect_car_license_plates.py
+++ detect_car_license_plates.py
@@ -3,6 +3,7 @@ warnings.filterwarnings('ignore')
 import read_video
 import logging
 import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
 
