9e0a63b692f245b7c5fea3790deb14a77a5a6262
Anna <anna.ovsi@mail.ru>
2020-10-07 17:38:36 +0300

    in developing..

--- regions.py
+++ regions.py
@@ -3,7 +3,5 @@ def load_regions():
     f = open('regions.txt', 'r', encoding='UTF-8')
     for line in f.readlines():
         tmp = line.split('\t')
-        all_region.append(tmp[1])
+        all_region.append(tmp[0])
     return all_region
-
-print(load_regions())
