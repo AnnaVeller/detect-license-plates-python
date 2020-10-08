a2a1fdf368fff4b0e91b1c8f77aa6b8d79af3989
Anna <anna.ovsi@mail.ru>
2020-10-08 19:25:45 +0300

    in developing..

--- detect_car_license_plates.py
+++ detect_car_license_plates.py
@@ -7,25 +7,7 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s:%(message)s')
 
-PATH_VIDEO = "test_mini.mp4"
-NAME = os.path.splitext(PATH_VIDEO)[0]
-logging.info(" Запустили видео %s" % PATH_VIDEO)
-cars = read_video.detect_one_video(PATH_VIDEO, NAME)
-logging.info(" Итоговый номер " + str(set(cars)) + "\n\n")
-
-PATH_VIDEO = "test_mini2.mp4"
-NAME = os.path.splitext(PATH_VIDEO)[0]
-logging.info(" Запустили видео %s" % PATH_VIDEO)
-cars = read_video.detect_one_video(PATH_VIDEO, NAME)
-logging.info(" Итоговый номер " + str(set(cars)) + "\n\n")
-
-PATH_VIDEO = "test_mini3.mp4"
-NAME = os.path.splitext(PATH_VIDEO)[0]
-logging.info(" Запустили видео %s" % PATH_VIDEO)
-cars = read_video.detect_one_video(PATH_VIDEO, NAME)
-logging.info(" Итоговый номер " + str(set(cars)) + "\n\n")
-
-PATH_VIDEO = "test_mini4.mp4"
+PATH_VIDEO = "test_multy.mp4"
 NAME = os.path.splitext(PATH_VIDEO)[0]
 logging.info(" Запустили видео %s" % PATH_VIDEO)
 cars = read_video.detect_one_video(PATH_VIDEO, NAME)
