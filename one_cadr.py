4ecae634f37be4d93972b22321af45e8c14b78da
Roman Strakhov <r.strakhov@bg-optics.ru>
2020-10-16 23:05:02 +0300

    Version=2.0 of all system. New file to process one cadr

--- one_cadr.py
+++ one_cadr.py
@@ -19,7 +19,7 @@ LightSkyBlue = (250, 206, 135)
 
 
 def process(frame, one_number, count, h):
-    state, really_number, number, status, cords, zones = model.detect_number(frame)
+    state, really_number, number, cords, zones = model.detect_number(frame)
 
     if state:
         for c in cords:
@@ -35,20 +35,20 @@ def process(frame, one_number, count, h):
     else:
         count += 1
 
+    car_number = 'no'
     if count < CADRS_TO_FIND_NEW_CAR:
         flag_new_car = False
         one_number.extend(number)  # список номер для текущей одной машины
         if len(one_number) >= MIN_CADRS_TO_DETECT:
-            name = wrong_numbers.wrong(one_number)
-            reg = regions.which_regions(name)
+            car_number = wrong_numbers.wrong(one_number)
+            reg = regions.which_regions(car_number)
             font = ImageFont.truetype(FONT, 32)
             img_pil = Image.fromarray(frame)
             draw = ImageDraw.Draw(img_pil)
             draw.text((20, h - 150), str(reg), font=font, fill=Red)  # fill=(0, 0, 255, 0)
             frame = np.array(img_pil)
-            cv2.putText(frame, str(name), (20, h - 80), cv2.FONT_HERSHEY_SIMPLEX, 1, Red, 2)
+            cv2.putText(frame, str(car_number), (20, h - 80), cv2.FONT_HERSHEY_SIMPLEX, 1, Red, 2)
     else:
         one_number.clear()
         flag_new_car = True
-
-    return frame, name, count, one_number, flag_new_car
+    return frame, car_number, count, one_number, flag_new_car
